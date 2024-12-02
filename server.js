e6960074239bc5a1303c9305a5777f9548f937e1
jjwc3 <jjwjjw2007@naver.com>
2024-12-03 01:03:26 +0900

    241203

--- server.js
+++ server.js
@@ -6,13 +6,19 @@ const { Server } = require('socket.io');
 const port = 8087;
 
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "*", // 클라이언트가 실행 중인 URL
+        methods: ["GET", "POST"],
+    },
+});
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
+        console.log(msg);
     });
 
     socket.on('disconnect', () => {
